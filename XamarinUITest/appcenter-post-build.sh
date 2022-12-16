d84e66680a619432ce0facd4f72df83e2d47ac85
tdevere <tdevere@microsoft.com>
2022-12-15 20:47:05 -0600

    changed

--- XamarinUITest/appcenter-post-build.sh
+++ XamarinUITest/appcenter-post-build.sh
@@ -30,8 +30,8 @@ appcenter login --token $AppCenterTokenForTest
 echo "Start App Center Test Run"
 echo "--app $app --devices $devices --app-path $apppath --test-series $testseries --locale $locale --build-dir $builddir --uitest-tools-dir $uitesttoolsdir"
 
-#appcenter test run uitest --app "$app" --devices "$devices" --app-path "$apppath" --test-series "$testseries" --locale "$locale" --build-dir "$builddir" --uitest-tools-dir "$uitesttoolsdir" -v
+appcenter test run uitest --app "$app" --devices "$devices" --app-path "$apppath" --test-series "$testseries" --locale "$locale" --build-dir "$builddir" --uitest-tools-dir "$uitesttoolsdir" --debug
 
-appcenter test run uitest --app "AppCenterTestProjects/Xamarin_Build_Script_CLI" --devices "Xamarin_Build_Script_CLI/Samsung" --app-path "/Users/runner/work/1/a/build/com.companyname.xamarinuitest.apk" --test-series "Samples" --locale "en_US" --build-dir "/Users/runner/work/1/s/XamarinUITest/UITest/UITest/bin/Release/" --uitest-tools-dir "/Users/runner/.nuget/packages/xamarin.uitest/3.2.9/tools/" -v
+appcenter test run uitest --app "AppCenterTestProjects/Xamarin_Build_Script_CLI" --devices "Xamarin_Build_Script_CLI/Samsung" --app-path "/Users/runner/work/1/a/build/com.companyname.xamarinuitest.apk" --test-series "Samples" --locale "en_US" --build-dir "/Users/runner/work/1/s/XamarinUITest/UITest/UITest/bin/Release/" --uitest-tools-dir "/Users/runner/.nuget/packages/xamarin.uitest/3.2.9/tools/" --debug
 
 echo "The Post Build Script END"
